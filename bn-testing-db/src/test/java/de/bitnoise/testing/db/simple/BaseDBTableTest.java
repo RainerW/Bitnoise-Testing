ff40e6a7c4af389c096f590943d0618e5c8bc154
RainerW <git@inforw.de>
2011-11-13 17:31:48 +0100

    0.0.1 features

---
+++ bn-testing-db/src/test/java/de/bitnoise/testing/db/simple/BaseDBTableTest.java
@@ -0,0 +1,31 @@
+package de.bitnoise.testing.db.simple;
+
+import static org.fest.assertions.Assertions.*;
+
+import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.datatype.DataType;
+import org.junit.Test;
+
+import de.bitnoise.testing.db.simple.BaseDBTable;
+
+public class BaseDBTableTest
+{
+  @Test
+  public void testAddRow() throws Exception
+  {
+    // preapre
+    BaseDBTable sut = new BaseDBTable("Eine Tabelle_01");
+    sut.addColumn("column 1 ", DataType.VARCHAR);
+
+    // execute
+    ITable table = sut.getTable();
+
+    // verify
+    assertThat(table.getRowCount()).isEqualTo(0);
+    assertThat(table.getTableMetaData()).isNotNull();
+    assertThat(table.getTableMetaData().getTableName()).isEqualTo("Eine Tabelle_01");
+    assertThat(table.getTableMetaData().getColumns()).hasSize(1);
+    assertThat(table.getTableMetaData().getColumnIndex("column 1 ")).isEqualTo(0);
+  }
+
+}
---
+++ bn-testing-db/src/test/java/de/bitnoise/testing/db/simple/ValueBuilderTest.java
@@ -0,0 +1,20 @@
+package de.bitnoise.testing.db.simple;
+
+import de.bitnoise.testing.db.simple.BaseDBTable;
+
+public class ValueBuilderTest
+{
+  Object _lastSetValue;
+  int _lastSetIndex;
+
+  BaseDBTable table = new BaseDBTable("")
+  {
+    public void setValue(int index, Object value)
+    {
+
+    }
+
+    ;
+  };
+
+}
